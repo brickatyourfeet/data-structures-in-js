e57fe42d69db0bec9cbcd25189cd518da0af2c4a
brickatyourfeet <brickatyourfeet@gmail.com>
2019-05-07 14:43:37 -0700

    add shift

--- singly-linked-list.js
+++ singly-linked-list.js
@@ -40,4 +40,14 @@ class SinglyLinkedList{
         }
         return current;
     }
+    shift(){
+        if(!this.head) return undefined;
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if(this.length === 0){
+            this.tail = null;
+        }
+        return currentHead;
+    }
 }
