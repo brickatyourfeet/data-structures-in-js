00225c7826012993b1110fc85d1386818dd949ec
brickatyourfeet <brickatyourfeet@gmail.com>
2019-05-08 21:32:43 -0700

    add unshift

--- singly-linked-list.js
+++ singly-linked-list.js
@@ -50,4 +50,15 @@ class SinglyLinkedList{
         }
         return currentHead;
     }
+    unshift(val){
+        var newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        }
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
 }
